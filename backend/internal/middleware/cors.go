1a87719fc964a412b95cf5a4c61743cee08fd515
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2024-09-28 18:26:41 +0300

    chore: cors policy

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func CORS(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://pokkne.fi")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
